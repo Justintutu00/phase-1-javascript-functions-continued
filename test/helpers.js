985720fa0a121a7bf56d0947a1bf124891c0b408
justintutu <jstntutu3@gmail.com>
2025-03-23 00:33:29 +0300

    Updated index.js, test files, and package configurations

--- test/helpers.js
+++ test/helpers.js
@@ -1,20 +1,27 @@
-const chai = require('chai')
-global.expect = chai.expect
-const fs = require('fs')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
-const babel = require('babel-core');
+console.log("Loading helpers.js...");
 
-const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
-
-const babelResult = babel.transformFileSync(
-  path.resolve(__dirname, '..', 'index.js'), {
-    presets: ['env']
-  }
-);
-
-const src = babelResult.code
-
-jsdom({
-  html, src
+const chai = require("chai");
+const sinon = require("sinon");
+global.expect = chai.expect;
+const fs = require("fs");
+const path = require("path");
+const babel = require("@babel/core");
+require("@babel/register")({
+  presets: ["@babel/preset-env"],
 });
+
+console.log("Reading HTML file...");
+const html = fs.readFileSync(path.resolve(__dirname, "..", "index.html"), "utf-8");
+
+console.log("Transforming JavaScript file...");
+try {
+  require(path.resolve(__dirname, "..", "index.js"));
+  console.log("JavaScript transformed successfully.");
+} catch (error) {
+  console.error("Babel transformation failed:", error);
+}
+
+console.log("Setting up JSDOM...");
+require("jsdom-global")(html);
+
+console.log("Setup complete.");
